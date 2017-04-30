2522a85ba5973bf3ac979cd37ccf6740c4b01857
Anita Popescu <anita.popescu@gmail.com>
2017-04-30 10:29:03 -0400

    Added unpublish and modals

--- _includes/document.js
+++ _includes/document.js
@@ -81,6 +81,18 @@ function saveExistingDocument(filename, privateKey) {
 		documentRef : documentRef,
 		dateLastModified : dlm,
 	});
+
+	userRef.once("value",function(snapshot){
+		var isPub = snapshot.val().isPublished;
+		if(isPub){
+			var publicKey = snapshot.val().publishedKey;
+			var publicRef = firebase.database().ref('public').child(publishedKey);
+			publicRef.update({
+				documentRef : documentRef,
+				dateLastModified : dlm,
+			})
+		}
+	});	
 }
 
 function saveNewDocument(filename) {
@@ -119,6 +131,8 @@ function saveHandler (){
 		saveNewDocument (documentName);
 	else
 		saveExistingDocument (documentName, key);
+
+	showAlert("Document saved!","");
 }
 
 function getUserDocuments (divId) {
@@ -152,23 +166,36 @@ function getPublicDocuments (divId) {
 			var documentAuthor = childSnapshot.val().author;
 			var documentKey = childSnapshot.key;
 			var dateLastModified = childSnapshot.val().dateLastModified;
-			mergedHTML += "<div class='public-documents__document'><div class='public-documents__document-name'><a class='public-documents__link' href='/view.html?filename=" + encodeURI(documentName) + "&key=" + documentKey + "&author=" + encodeURI(documentAuthor) + "&datemodified=" + encodeURI(dateLastModified) + "'>" + documentName + "</a></div><div class='member-documents__date'>" + dateLastModified + "</div><div class='public-documents__author'>" + documentAuthor + "</div></div>";
+			mergedHTML += "<div class='public-documents__document'><div class='public-documents__document-name'><a class='public-documents__link' href='/view.html?filename=" + encodeURI(documentName) + "&key=" + documentKey + "&author=" + encodeURI(documentAuthor) + "&datemodified=" + encodeURI(dateLastModified) + "'>" + documentName + "</a></div><div class='public-documents__date'>" + dateLastModified + "</div><div class='public-documents__author'>" + documentAuthor + "</div></div>";
 		});
 		document.getElementById(divId).innerHTML = mergedHTML;
 	});
 }
 
 function publishHandler (){
+
 	var documentName = document.getElementById('document-name').innerHTML;
 	var key = getURLParam().key;
+	var uid = firebase.auth().currentUser.uid;
+
 	if (!key) {
 		console.log("couldn't find private key, creating new document");
 		key = saveNewDocument(documentName);
 		console.log("Key is:" + key);
+		console.log("handler says: "+documentName);
+		publishDocument (documentName, key);
+	} else {
+		var savedRef = firebase.database().ref('private').child(uid).child("documents").child(key);
+		savedRef.once("value",function(snapshot){
+			var isPub = snapshot.val().isPublished;
+			if(!isPub){
+				publishDocument (documentName, key);
+			}
+			else {
+				unpublishDocument(key);
+			}
+		});	
 	}
-		
-	console.log("handler says: "+documentName);
-	publishDocument (documentName, key);
 }
 
 function publishDocument(filename, privateKey) {
@@ -202,12 +229,29 @@ function publishDocument(filename, privateKey) {
 	})
 
 	var savedRef = firebase.database().ref('private').child(uid).child("documents").child(privateKey);
-
+	console.log("published key is"+newDocRef.key);
 	savedRef.update({
 		isPublished :true,
 		publishedKey : newDocRef.key,
+		documentRef : documentRef,
 	});
+	showAlert("Congratulations! You have published this document.", "You can view it at <a href='/view.html?filename=" + encodeURI(title) + "&key=" + newDocRef.key + "&author=" + encodeURI(author) + "&datemodified=" + encodeURI(dlm) + "'>" + title + "</a>.");
+	});
+}
 
+function unpublishDocument(privateKey) {
+	var publicRef = firebase.database().ref('public').child(privateKey);
+	publicRef.delete().then(function() {
+		showAlert("You have successfully unpublished this document.", "You can still view your file in your 'My documents' tab.");
+		
+		var uid = firebase.auth().currentUser.uid;
+		var savedRef = firebase.database().ref('private').child(uid).child("documents").child(privateKey);
+		savedRef.update({
+			isPublished :false,
+			publishedKey : "",
+		});
+	}).catch(function(error) {
+  		console.log("error unpublishing document.");
 	});
 }
 
